1b5a7b8519a43f11cb121ba852c16001e31e3b1a
Kutay <kutay.kagan.ozenn@gmail.com>
2023-07-05 19:00:07 +0300

    done.

--- app.js
+++ app.js
@@ -6,6 +6,19 @@ app.get('/', (req,res) => {
     res.send('Hello Worlddddd')
 })
 
+app.post('/', (req,res) => {
+    res.send('I got a Post request')
+})
+
+app.put('/user', (req, res) => {
+    res.send('I got a Put request on the /user route')
+})
+
+app.delete('/user', (req, res) => {
+    res.send('I got a Delete request at /user route')
+})
+
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
